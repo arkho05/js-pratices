f1e60a5cc81e0df8d4c6389df077fc0855e0c22a
arkho05 <123075443+arkho05@users.noreply.github.com>
2023-10-13 22:47:30 +0000

    un_compleate

---
+++ 01_mini_project.js
@@ -0,0 +1,8 @@
+/* make a rectangle and when we move mouse on the rectangle on
+the left side we want to gradually6 change the color of that 
+rectangle to red and when we move mouse to the right side we want to change 
+the color to blue */
+
+
+
+ 
