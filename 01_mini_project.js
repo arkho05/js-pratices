995481e1ed62bec9c3e6dd9e5c3c15157b6a4278
arkho05 <123075443+arkho05@users.noreply.github.com>
2023-10-14 21:03:09 +0000

    mini_js

--- 01_mini_project.js
+++ 01_mini_project.js
@@ -3,6 +3,20 @@ the left side we want to gradually6 change the color of that
 rectangle to red and when we move mouse to the right side we want to change 
 the color to blue */
 
+var rect = document.querySelector("#center");//dom
 
+rect.addEventListener("mousemove", function(box){
+    var rectanglelocation =rect.getBoundingClientRect();
+    var insiderectval = box.clientx - rectanglelocation.left;
+    
+    if(insiderectval<rectanglelocation.width/2){
+        console.log("left");
+    }
+    else {
+        console.log("right");
+    }
+})
+
+ 
 
  
